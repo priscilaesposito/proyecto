676f550ea5ebeecbad6d202a6f72331853a7e5fc
priscilaesposito <espositopriscila626@gmail.com>
2025-10-23 18:12:50 -0300

    pri

---
+++ ejecutar.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\Ruth\GitHubRep\proyecto"
+java -cp "projecto/lib/sqlite-jdbc-3.44.1.0.jar;projecto/lib/slf4j-api-2.0.9.jar;projecto/lib/slf4j-simple-2.0.9.jar;C:/Users/Ruth/AppData/Roaming/Code/User/workspaceStorage/1660d03f3310413e0de88ba53fd48006/redhat.java/jdt_ws/proyecto_fe2a8bd3/bin" app.Main
+pause
