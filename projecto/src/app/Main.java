3ac2ca32825c4175d9f3b424b880b5aeac9ff493
Juana Sabbione <sabbionejuana@gmail.com>
2025-10-26 18:31:13 -0300

    jua

--- projecto/src/app/Main.java
+++ projecto/src/app/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 import DAO.DatosPersonalesDAO;
 import DAO.UsuarioDAO;
 import DAO.conexion;
-import DAOjdbc.DatosPersonalesDAOjdbc;
+import DAOjdbc.DatosPersonalesDAOJdbc;
 import model.GestionUsuario;
 import DAOjdbc.UsuarioDAOjdbc;
 import DB.BaseDeDatos;
@@ -20,7 +20,7 @@ import java.util.Arrays;
 public class Main {
 
     private static UsuarioDAO usuarioDAO = new UsuarioDAOjdbc();
-    private static DatosPersonalesDAO datosPersonalesDAO = new DatosPersonalesDAOjdbc();
+    private static DatosPersonalesDAO datosPersonalesDAO = new DatosPersonalesDAOJdbc();
     private static Scanner scanner = new Scanner(System.in);
     private static GestionUsuario gestionUsuario = new GestionUsuario();
 
