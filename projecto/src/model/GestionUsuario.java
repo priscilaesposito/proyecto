a2a81e9576fa97a0124c41d85e3c54f9b983f75d
priscilaesposito <espositopriscila626@gmail.com>
2025-10-26 18:29:36 -0300

    pri

--- projecto/src/model/GestionUsuario.java
+++ projecto/src/model/GestionUsuario.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 import DAO.DatosPersonalesDAO;
-import DAOjdbc.DatosPersonalesDAOjdbc;
+import DAOjdbc.DatosPersonalesDAOJdbc;
 
 import DAO.UsuarioDAO;
 import DAOjdbc.UsuarioDAOjdbc;
