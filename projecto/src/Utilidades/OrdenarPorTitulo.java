0090662d87841b159200cbd7bfbe27ed5962d561
Juana Banana <sabbionejuana@gmail.com>
2025-10-26 23:53:53 -0300

    jua

--- projecto/src/Utilidades/OrdenarPorTitulo.java
+++ projecto/src/Utilidades/OrdenarPorTitulo.java
@@ -7,9 +7,6 @@ public class OrdenarPorTitulo implements Comparator<Pelicula> {
 
     @Override
     public int compare(Pelicula p1, Pelicula p2) {
-        // Se ordena alfabéticamente por el Título, obtenido del objeto metadatos.
-        // Asume que getMetadatos() y getTitulo() no son null (esto debe ser validado
-        // antes).
         return p1.getMetadatos().getTitulo().compareTo(p2.getMetadatos().getTitulo());
     }
 }
