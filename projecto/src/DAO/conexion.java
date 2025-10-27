6d13b58005abb15ce302cbbdc47a7c7ba828ac13
priscilaesposito <espositopriscila626@gmail.com>
2025-10-27 13:15:09 -0300

    pri

--- projecto/src/DAO/conexion.java
+++ projecto/src/DAO/conexion.java
@@ -5,30 +5,40 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexion {
-            
-    private static final String DB_URL = "jdbc:sqlite:plataforma_streaming.db";
+private static final String DB_URL = "jdbc:sqlite:plataforma_streaming.db";
 
-    public static Connection conectar() throws SQLException {
-        Connection connection = null;
+    private static Connection connection = null;
+
+    //DRIVER
+    static {
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection(DB_URL);
         } catch (ClassNotFoundException e) {
-            System.out.println("Error: Driver JDBC de SQLite no encontrado: " + e.getMessage());
-            throw new SQLException("Driver JDBC no encontrado", e);
+            System.err.println("Error: Driver JDBC de SQLite no encontrado: " + e.getMessage());
+            throw new RuntimeException("Driver JDBC no encontrado", e);
+        }
+    }
+    
+   
+    public static synchronized Connection conectar() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            //CREA NUEVA
+            connection = DriverManager.getConnection(DB_URL);
         }
         return connection;
     }
 
-    public static void desconectar() {
+    
+    public static synchronized void desconectar() {
         try {
-            Connection connection = conectar();
-            if (connection != null && !connection.isClosed()) {
+             if (connection != null && !connection.isClosed()) {
                 connection.close();
+                connection = null; 
+                System.out.println("Conexión a la base de datos cerrada.");
             }
         } catch (SQLException e) {
             System.out.println("Error al desconectar de la base de datos SQLite: " + e.getMessage());
         }
     }
-
 }
+
