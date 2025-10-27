f918e044bef6698676d7b1d8ac11e090052c2742
priscilaesposito <espositopriscila626@gmail.com>
2025-10-26 23:51:41 -0300

    Pri

--- projecto/src/model/Pelicula.java
+++ projecto/src/model/Pelicula.java
@@ -4,6 +4,11 @@ public class Pelicula extends Titulo {
     private Video video;
     private int ID;
 
+    // Constructor que inicializa el objeto Video
+    public Pelicula() {
+        this.video = new Video();
+    }
+
     public int getID() {
         return ID;
     }
