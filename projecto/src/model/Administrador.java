cb241a8f3ea8e310aeb3e4981ae5f6450f219db8
Juana Banana <sabbionejuana@gmail.com>
2025-10-27 00:02:33 -0300

    jua

--- projecto/src/model/Administrador.java
+++ projecto/src/model/Administrador.java
@@ -31,7 +31,6 @@ public class Administrador extends Usuario {
      * @param T Titulo a dar de baja.
      */
     public void darDeBaja(Titulo T) {
-        // Lógica para dar de baja el título
     }
 
     /**
@@ -40,16 +39,14 @@ public class Administrador extends Usuario {
      * @param T Titulo a dar de alta.
      */
     public void darAlta(Titulo T) {
-        // Lógica para dar de alta el título
     }
 
     /**
-     * Modifica la información de un titulo existente.
+     * Modifica la informacion de un titulo existente.
      *
      * @param T Titulo a modificar.
      */
     public void modificar(Titulo T) {
-        // Lógica para modificar el título
     }
 
     public void validarRegistroPelicula(Pelicula pelicula) {
